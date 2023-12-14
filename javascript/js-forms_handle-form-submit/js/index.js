93223ae3eaeb803f52dc6536d4374762bacdb51a
jamoragar <javier.moragarojas@gmail.com>
2023-12-14 10:15:55 +0100

    challenge[handle_form_submit]: Challenge solved.

--- javascript/js-forms_handle-form-submit/js/index.js
+++ javascript/js-forms_handle-form-submit/js/index.js
@@ -1,3 +1,12 @@
 console.clear();
 
 const form = document.querySelector('[data-js="form"]');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData);
+
+    console.log(data);
+})
