3e52c31ccd3d129534b6db5a074bfc19321664e2
jamoragar <javier.moragarojas@gmail.com>
2023-12-15 10:47:24 +0100

    challenge[js_toastMessage]: Challenge done

--- javascript/js-createelement_toast-messages/js/index.js
+++ javascript/js-createelement_toast-messages/js/index.js
@@ -5,9 +5,13 @@ const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
 addButton.addEventListener("click", () => {
-  // Exercise: Append a new entry to the toast messages container
+  const newToast = document.createElement('ol');
+  newToast.classList.add('toast-container__message');
+  newToast.textContent = 'This is a new toast message';
+
+  toastContainer.appendChild(newToast);
 });
 
 clearButton.addEventListener("click", () => {
-  // Exercise: Clear the stack of toast messages
+  toastContainer.innerHTML = '' 
 });
