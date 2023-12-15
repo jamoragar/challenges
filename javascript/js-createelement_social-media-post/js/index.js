8f68221df392652dedd446f743905bcf73a5bbfd
jamoragar <javier.moragarojas@gmail.com>
2023-12-15 10:56:36 +0100

    challenge[js_socialMediaPost]: Challenge done

--- javascript/js-createelement_social-media-post/js/index.js
+++ javascript/js-createelement_social-media-post/js/index.js
@@ -10,3 +10,37 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
+
+
+// Creating the tags and adding classes
+const secondPost = document.createElement('section');
+secondPost.classList.add('post');
+
+const postContent = document.createElement('p');
+postContent.classList.add('post__content');
+postContent.textContent = 'Another post content...';
+
+const postFooter = document.createElement('footer');
+postFooter.classList.add('post__footer');
+
+const usernameSpan = document.createElement('span');
+usernameSpan.classList.add('post__username');
+usernameSpan.textContent = '@anotheruser';
+
+const likeButtonSecondPost = document.createElement('button');
+likeButtonSecondPost.classList.add('post__button');
+likeButtonSecondPost.setAttribute('type', 'button');
+likeButtonSecondPost.setAttribute('data-js', 'like-button');
+likeButtonSecondPost.textContent = '♥ Like';
+
+// Adding the functionallity to the 'like button'
+likeButtonSecondPost.addEventListener('click', handleLikeButtonClick);
+
+postFooter.appendChild(usernameSpan);
+postFooter.appendChild(likeButtonSecondPost);
+
+secondPost.appendChild(postContent);
+secondPost.appendChild(postFooter);
+
+document.body.appendChild(secondPost);
+
