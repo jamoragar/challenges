3cd104a09b59131d85e1e3b26a4bee5365e0ef85
jamoragar <javier.moragarojas@gmail.com>
2023-12-18 17:29:48 +0100

    challenge[js_stars]: Challenge done

--- javascript/js-loops_stars/js/index.js
+++ javascript/js-loops_stars/js/index.js
@@ -2,13 +2,24 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(filledStars) {
   // reset the star container before rerendering stars
   starContainer.innerHTML = "";
 
   //--v-- your code here --v--
-
+  for (let i = 1; i <= 5; i++) {
+    const star = document.createElement('img');
+    star.setAttribute('src', i <= filledStars ?
+      'assets/star-filled.svg'
+      :
+      'assets/star-empty.svg');
+    star.addEventListener('click', function () {
+      renderStars(i);
+    });
+    starContainer.appendChild(star);
+  }
+};
   //--^-- your code here --^--
-}
 
-renderStars();
+
+renderStars(5);
