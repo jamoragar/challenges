8c6f11d5ef4b5c54cebc1055ce7854d06936b85d
jamoragar <javier.moragarojas@gmail.com>
2024-01-07 18:41:10 +0100

    challenge[jsAsyncFunctions_sleep]: Challenge Done

--- javascript/js-async-functions_sleep/js/index.js
+++ javascript/js-async-functions_sleep/js/index.js
@@ -8,17 +8,19 @@ const body = document.body;
 
 button3.addEventListener("click", async () => {
   icon.textContent = "💤";
-  sleep(3);
+  await sleep(3);
   icon.textContent = "⏰";
 });
 
-button5.addEventListener("click", () => {
+button5.addEventListener("click", async () => {
   icon.textContent = "💤";
+  await sleep(5);
   icon.textContent = "⏰";
 });
 
-function handleSleepFor10Button() {
+async function handleSleepFor10Button() {
   icon.textContent = "💤";
+  await sleep(10);
   icon.textContent = "⏰";
 }
 
