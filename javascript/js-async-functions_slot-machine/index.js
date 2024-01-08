95dd6f3db0ecff6207d508a33b80681dfd5d5452
jamoragar <javier.moragarojas@gmail.com>
2024-01-08 14:07:30 +0100

    challenge[jsAsyncFunctions_slotMachine]: Challenge Done

--- javascript/js-async-functions_slot-machine/index.js
+++ javascript/js-async-functions_slot-machine/index.js
@@ -2,6 +2,7 @@ import { Wheel } from "./components/Wheel/Wheel.js";
 import { SpinButton } from "./components/SpinButton/SpinButton.js";
 import { Machine } from "./components/Machine/Machine.js";
 import { Result } from "./components/Result/Result.js";
+import { getMaxCount } from './utils/symbols.js';
 
 console.clear();
 
@@ -62,6 +63,23 @@ spinButton.addEventListener("click", async () => {
    * and make sure it is always executed after the wheels have stopped,
    * even if an error was thrown.
    */
+  try{
+    result.setSpinning(),
+    await Promise.all([
+      wheel1.spin(),
+      wheel2.spin(),
+      wheel3.spin()
+    ]).then(val => {
+      const symbols = getMaxCount(val);
+
+      result.setResult(symbols === 3 ? 100 : symbols === 2 ? 10 : 0)
+  
+    })
+
+  }catch(err){
+    console.log(err)
+    result.setMachineChoked()
+  }
 
   spinButton.disabled = false;
 });
