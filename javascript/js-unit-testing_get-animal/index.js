780956aa9559c5e2554a7d1f36dbe9ea6626a058
jamoragar <javier.moragarojas@gmail.com>
2024-01-09 14:19:17 +0100

    challenge[jsUnitTest_getAnimal]: Challenge Done

--- javascript/js-unit-testing_get-animal/index.js
+++ javascript/js-unit-testing_get-animal/index.js
@@ -1,1 +1,7 @@
-export function getAnimal() {}
+export function getAnimal(animal) {
+    return animal === 'cats'
+    ? 'I totally love cats!'
+    : animal
+    ? `I like ${animal}!`
+    : 'I do not like animals at all!';
+  }
