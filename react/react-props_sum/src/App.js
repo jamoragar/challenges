9cb10e8da071e44054965041815939f4d6c35b02
jamoragar <javier.moragarojas@gmail.com>
2024-01-15 14:15:36 +0100

    challenge[reactProps_sum]: Challenge Done

--- react/react-props_sum/src/App.js
+++ react/react-props_sum/src/App.js
@@ -2,5 +2,15 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Sum valueA={10} valueB={15} />;
+}
+
+function Sum(props) {
+  const { valueA, valueB } = props;
+  const result = valueA + valueB;
+  return (
+    <h3>
+      {valueA} + {valueB} = {result}
+    </h3>
+  );
 }
