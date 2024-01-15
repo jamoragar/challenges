624834b6ba60b93c0e34c46d320015e77e38c5ca
jamoragar <javier.moragarojas@gmail.com>
2024-01-15 10:07:36 +0100

    challenge[react_attributes]: Challenge Done

--- react/react-basics_attributes/src/App.js
+++ react/react-basics_attributes/src/App.js
@@ -2,5 +2,21 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <Article />;
+}
+
+function Article() {
+  return (
+    <article>
+      <h2 className="article__title">My title!</h2>
+      <label htmlFor="myInput">My Input</label>
+      <input id="myInput" type="text" name="myInput" />
+      <a
+        className="article__link"
+        href="https://en.wikipedia.org/wiki/Mega_Man"
+      >
+        Mega Man
+      </a>
+    </article>
+  );
 }
