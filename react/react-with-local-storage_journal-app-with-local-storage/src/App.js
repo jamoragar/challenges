ef7c20b45168e158503f25488fa0087a6b3785e5
jamoragar <javier.moragarojas@gmail.com>
2024-01-22 11:16:50 +0100

    challenge[reactlocalStorage]: Challenge Journal App - Done

--- react/react-with-local-storage_journal-app-with-local-storage/src/App.js
+++ react/react-with-local-storage_journal-app-with-local-storage/src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import useLocalStorageState from "use-local-storage-state";
 import "./App.css";
 import EntriesSection from "./components/EntriesSection";
 import EntryForm from "./components/EntryForm";
@@ -37,7 +38,9 @@ const initialEntries = [
 ];
 
 function App() {
-  const [entries, setEntries] = useState(initialEntries);
+  const [entries, setEntries] = useLocalStorageState("entries", {
+    defaultValue: initialEntries,
+  });
   const [filter, setFilter] = useState("all"); // "all" or "favorites"
 
   function handleAddEntry(newEntry) {
